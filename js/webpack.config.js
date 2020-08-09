c3cac0757050ac050cf3aab816d6eee6a6da0865
Oliver Cordes <ocordes@astro.uni-bonn.de>
2020-08-09 15:24:03 +0200

    - test some webpack things

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -3,20 +3,24 @@ const path = require('path');
 module.exports = {
 	//mode: 'development',
         mode: 'production',
-	entry: './src/index.js',
+	//entry: './src/index.js',
+        entry: './build/index.js',
+        //devtool: 'source-map',
+        devtool: false,
+        performance: {
+ 	       hints: false,
+               maxEntrypointSize: 512000,
+               maxAssetSize: 512000
+        },
 	output: {
 	  filename: 'main.js',
-          path: path.resolve(__dirname, '../src/app/static'),
+          path: path.resolve(__dirname, '../src/app/static/desk'),
         },
 	module: {
 	   rules: [
-	     {
-                test: /\.css$/,
-                use: [
-                  'style-loader',
-                  'css-loader',
-                ],
-             },
+	     { test: /\.css$/, use: ['style-loader', 'css-loader' ] },
+	     { test: /\.js$/, use: ["source-map-loader"], enforce: "pre" },
+	     { test: /\.png$/, use: 'file-loader' }
            ],
        },
 };
